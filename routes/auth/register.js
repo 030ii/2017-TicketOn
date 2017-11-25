f01720a0856f05fa1debb6fa6fd5d2103b5fdc62
jiho <cko13@naver.com>
2017-11-25 20:58:59 +0900

    경로수정
    js

--- routes/auth/register.js
+++ routes/auth/register.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('register',{});
 });
