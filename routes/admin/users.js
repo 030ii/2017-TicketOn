ad7358f109e270a46d906d577c296015b600ff22
stafor94 <stafor@naver.com>
2017-11-28 00:45:37 +0900

    users 오류해결

--- routes/admin/users.js
+++ routes/admin/users.js
@@ -4,36 +4,26 @@ var pool = require('../../config.js').pool;
 
 
 router.get('/', function(req,res,next){
-
     pool.getConnection(function (err, connection) {
-        // Use the connection
         var query = "SELECT * FROM user";
         connection.query(query, function (err, rows) {
             if (err) console.error("err : " + err);
             console.log("rows : " + JSON.stringify(rows));
-
             res.render('admin/users', {rows: rows});
             connection.release();
-
-            // Don't use the connection here, it has been returned to the pool.
         });
     });
 });
 
-router.get('/users/:uid', function(req,res,next){
-console.log(req.params.uid);
+router.get('/:uid', function(req,res,next){
     pool.getConnection(function (err, connection) {
-        // Use the connection
         var query = "DELETE FROM user where uid=?";
         console.log(req.params.uid);
         connection.query(query, req.params.uid, function (err, rows) {
             if (err) console.error("err : " + err);
-            console.log("rows : " + JSON.stringify(rows));
-
-            res.redirect('admin/users', {rows: rows});
+            console.log("rows : " + rows);
+            res.redirect('admin/users');
             connection.release();
-
-            // Don't use the connection here, it has been returned to the pool.
         });
     });
 });
