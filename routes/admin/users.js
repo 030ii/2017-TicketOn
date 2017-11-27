38e83dad8cc79ed90131dd7ed87c572278a00ea9
jiho <cko13@naver.com>
2017-11-28 01:08:51 +0900

    삭제완료 크~

--- routes/admin/users.js
+++ routes/admin/users.js
@@ -18,12 +18,22 @@ router.get('/', function(req,res,next){
 router.get('/:uid', function(req,res,next){
     pool.getConnection(function (err, connection) {
         var query = "DELETE FROM user where uid=?";
+        var querylist = "SELECT * FROM user";
         console.log(req.params.uid);
         connection.query(query, req.params.uid, function (err, rows) {
             if (err) console.error("err : " + err);
             console.log("rows : " + rows);
-            res.redirect('admin/users');
             connection.release();
+
+            pool.getConnection(function (err, connection) {
+                connection.query(querylist, function (err, rows) {
+                    if (err) console.error("err : " + err);
+                    console.log("rows : " + JSON.stringify(rows));
+                    res.render('admin/users', {rows: rows});
+                    connection.release();
+                });
+            });
+
         });
     });
 });
