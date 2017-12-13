392699bebb6c6df4ebaba7e90264452286378b0d
stafor94 <stafor@naver.com>
2017-12-13 12:37:58 +0900

    경매 등록 오류 수정

--- routes/auction/post.js
+++ routes/auction/post.js
@@ -13,10 +13,10 @@ router.post('/', upload.single('img'), function(req, res, next) {
     var body = req.body;
     var date = new Date();
     date.setTime(date.getTime() + (Number(body.deadline) * 1000 * 60 * 60));
-    var queryStr = 'INSERT INTO auction(uid, a_title, a_content, a_img, a_min_price, a_deadline)'
-                  + ' VALUES(?,?,?,?,?,?)';
-    var inputs = [req.session.uid, body.title, body.content, req.file.filename,
-                    body.minPrice, date];
+    var queryStr = 'INSERT INTO auction(uid, a_category, a_title, a_content, a_img, a_min_price, a_deadline)'
+                  + ' VALUES(?,?,?,?,?,?,?)';
+    var inputs = [req.session.uid, body.category, body.title, body.content,
+                  req.file.filename, body.minPrice, date];
     pool.getConnection(function(err, connection) {
         connection.query(queryStr, inputs, function(err, rows) {
             if(err) console.log("err: ", err);
