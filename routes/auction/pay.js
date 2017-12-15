df0783c6ea0605a043a666bd12c0fdcf6c417ec0
stafor94 <stafor@naver.com>
2017-12-16 07:22:00 +0900

    결제기능 추가

--- routes/auction/pay.js
+++ routes/auction/pay.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var pool = require('../../config.js').pool;
+var async = require('async');
 
-router.get('/', function(req, res, next) {
-
+router.post('/', function(req, res, next) {
+    var body = req.body;
+    pool.getConnection(function(err, connection) {
+        query = "INSERT INTO pay(uid,aid,p_price) VALUES(?,?,?)";
+        connection.query(query, [req.session.uid, body.aid, body.price], function(err) {
+            if(err) console.log(err);
+            res.send(true);
+            connection.release();
+        });
+    });
 });
 
 module.exports = router;
